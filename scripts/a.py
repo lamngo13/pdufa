07daa1c9ea8de6150a1d6f55164d769dd6a8b33d
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2026-02-01 11:28:25 -0500

    Update a.py

--- scripts/a.py
+++ scripts/a.py
@@ -14,16 +14,19 @@ with open(secrets_path, "r") as f:
 print(key)
 print(secret)
 
-endpoint = "https://paper-api.alpaca.markets/v2"
+APCA_API_BASE_URL = "https://paper-api.alpaca.markets" # For paper trading
+
+
 # No keys required for crypto data
 client = CryptoHistoricalDataClient()
 
 # Creating request object
 request_params = CryptoBarsRequest(
-  symbol_or_symbols=["BTC/USD"],
+  symbol_or_symbols=["AQST/USD"],
+  #symbol_or_symbols=["BTC/USD"],
   timeframe=TimeFrame.Day,
-  start=datetime(2022, 9, 1),
-  end=datetime(2022, 9, 7)
+  start=datetime(2025, 9, 1),
+  end=datetime(2025, 9, 7)
 )
 
 # Retrieve daily bars for Bitcoin in a DataFrame and printing it
